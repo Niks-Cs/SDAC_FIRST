b2bf718ea06f8279d378cb573079025a1362f23b
sdac-10 <sdac-10@sdac-10-PC>
2024-02-02 16:28:39 +0530

    my token

--- Main/src/nikhil/New.java
+++ Main/src/nikhil/New.java
@@ -3,6 +3,7 @@ package nikhil;
 public class New {
 	public static void main(String[] args) {
 		System.out.println("HELLO WORLD");
+		System.out.println("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");
 	}
 
 }
